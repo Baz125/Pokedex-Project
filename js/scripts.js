4fa4aeeccb3faab9ddb741a65ed73ec4e5cb749d
TiknockTokomak <103266154+TiknockTokomak@users.noreply.github.com>
2023-04-13 19:13:34 +0200

    Created 3 Variables, each describing a pokemon. Created one array containing each 3 variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
-alert('Hello world');
-let favouriteFood = "Pie";
-document.write(favouriteFood)
+
+
+let venusaur = [
+    {name: 'Venusaur'},
+    {height: 2},
+    ['grass', 'poison']
+  ];
+
+let slowbro = [
+  {name: 'Slowbrow'},
+  {height: 1.6},
+  ['psychic', 'water']
+];
+
+let gyrados = [
+  {name: 'Gyrados'},
+  {height: 6.5},
+  ['water', 'flying']
+]
+
+let pokemonList = [
+  venusaur,
+  slowbro,
+  gyrados
+];

edc1d89c3b1eac175c202757f0f3e10f036fb28a
TiknockTokomak <103266154+TiknockTokomak@users.noreply.github.com>
2023-04-19 16:48:44 +0200

    Changed for loop to forEach as per exercise 1.5
    removed conditional comment for tall pokemon (becuase I didn't know how to include it)
    
    added the printing of the types

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,48 @@ let pokemonList = [
   {
     name: "Venusaur",
     height: 2,
-    types: ["grass", "poison"],
+    types: [" grass", " poison"],
   },
   {
     name: "Slowbro",
     height: 4,
-    types: ["psychic", "water"],
+    types: [" psychic", " water"],
   },
   {
     name: "Gyrados",
     height: 6.5,
-    types: ["water", "flying"],
+    types: [" water", " flying"],
   },
 ];
 
-//for loop iterating through the array until the end
-for (let i = 0; i < pokemonList.length; i++) {
-  const name = pokemonList[i].name;
-  const height = pokemonList[i].height;
-  //for pokemon with height less than 6 just print name and height
-  if (pokemonList[i].height < 6) {
-    document.write(
-      "<p class = 'pokemon-card'>" + name + " - Height: " + height + "</p>"
-    );
-    //for pokemon whose height is not less than 6 print name, height and extra message
-  } else {
-    document.write(
-      "<p class = 'pokemon-card'>" +
-        name +
-        " - Height: " +
-        height +
-        " - Wow! He's a <span class = 'big'>Big</span> Boy! " +
-        "</p>"
-    );
-  }
+function myLoopFunction (pokemon) {
+  document.write("<p class = 'pokemon-card'>" + pokemon.name + " - Height: " + pokemon.height + ". Types: " + pokemon.types + "</p>")
 }
+pokemonList.forEach(myLoopFunction);
+
+
+
+
+
+
+// //for loop iterating through the array until the end
+// for (let i = 0; i < pokemonList.length; i++) {
+//   const name = pokemonList[i].name;
+//   const height = pokemonList[i].height;
+//   //for pokemon with height less than 6 just print name and height
+//   if (pokemonList[i].height < 6) {
+//     document.write(
+//       "<p class = 'pokemon-card'>" + name + " - Height: " + height + "</p>"
+//     );
+//     //for pokemon whose height is not less than 6 print name, height and extra message
+//   } else {
+//     document.write(
+//       "<p class = 'pokemon-card'>" +
+//         name +
+//         " - Height: " +
+//         height +
+//         " - Wow! He's a <span class = 'big'>Big</span> Boy! " +
+//         "</p>"
+//     );
+//   }
+// }
